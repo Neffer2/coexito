7e5181bc7ff8f0eaed4fce720f1ca67c75a0f657
Neffer2 <n3f73r@gmail.com>
2024-12-18 23:53:02 -0500

    1. Se crea la migración recomendadores

---
+++ database/migrations/2024_12_19_045002_create_recomendadores.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('recomendadores', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('cedula');
+            $table->integer('celular')->unique();
+            $table->integer('correo')->unique();
+            $table->integer('ciudad');
+            $table->foreign('pdv_id')->references('id')->on('puntos_venta');
+            $table->foreignId('pdv_id');
+            $table->integer('puntos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('recomendadores');
+    }
+};
