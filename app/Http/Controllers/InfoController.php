12ebe05d3bec8e5261758d577490533b03a037b2
Sebastian <juansecapri@hotmail.com>
2024-12-18 23:11:47 -0500

    Endpoint getAgente

--- app/Http/Controllers/InfoController.php
+++ app/Http/Controllers/InfoController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Agente;
 
 class InfoController extends Controller
 {
-    //
+    public function getAgente($documento){
+        $agente = User::select('id', 'nombre', 'cedula')->where('cedula', $documento)->first();
+        return response()->json($agente);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InfoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 //Endpoints for the API
-
+Route::get('/get-agente/{documento?}', [InfoController::class, 'getAgente']); 
