24a917f5a49863bc841735759497f0bd52774c8a
Neffer2 <n3f73r@gmail.com>
2025-01-14 18:08:06 -0500

    1. AVances

--- resources/views/livewire/puntos-registrados.blade.php
+++ resources/views/livewire/puntos-registrados.blade.php
@@ -6,17 +6,12 @@
             <td>Estado</td>
             <td>Fecha</td>
         </tr>
-        {{-- @foreach ($registros as $registro)
+        @foreach ($registros as $registro)
             <tr>
                 <td>{{ $registro->pdv->nom_cliente }}</td>
                 <td>{{ $registro->estado_id }}</td>
                 <td>{{ $registro->created_at }}</td>
             </tr>
-        @endforeach --}}
-        <tr>
-            <td>11111</td>
-            <td>Activo</td>
-            <td>11-12-98: 12:00:00</td>
-        </tr>
+        @endforeach
     </table>
 </div>
