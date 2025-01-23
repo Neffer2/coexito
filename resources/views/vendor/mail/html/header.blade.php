ecff8cd9ada6d2cdc51cbf7a9970c0f5ad160a06
Sebastián Camargo <juansecapri@hotmail.com>
2025-01-23 09:04:51 -0500

    Ajustes de platataforma

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,8 +2,8 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Cóexitocontigo')
-<img src="{{ asset('assets/coexito-logo.png') }}" class="logo" alt="Cóexito">
+@if (trim($slot) === 'Coéxitocontigo')
+<img src="{{ asset('assets/coexito-logo.png') }}" class="logo" alt="Coéxito">
 @else
 {{ $slot }}
 @endif
