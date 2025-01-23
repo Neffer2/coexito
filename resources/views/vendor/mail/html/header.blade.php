559d751a2115e79becc05f110bd93537802c7c09
Sebastián Camargo <juansecapri@hotmail.com>
2025-01-23 09:02:30 -0500

    Ajustes de mailing

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,8 +2,8 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
+@if (trim($slot) === 'Cóexitocontigo')
+<img src="{{ asset('assets/coexito-logo.png') }}" class="logo" alt="Cóexito">
 @else
 {{ $slot }}
 @endif
