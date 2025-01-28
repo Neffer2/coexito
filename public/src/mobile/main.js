3cc08c6bc51fdb0cb5271e07123d9f79f6a42f8c
Neffer2 <n3f73r@gmail.com>
2025-01-27 19:01:05 -0500

    1. CAmbios vbaios git puysh o

--- public/src/mobile/main.js
+++ public/src/mobile/main.js
@@ -18,7 +18,7 @@ const config = {
         default: 'arcade',
         arcade: {
             // gravity: { y: 1000 },
-            // debug: true
+            debug: true
         }
     },
     transparent: true
