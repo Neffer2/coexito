39a3c5563f8e02e1a8c688dd9e84d9d613c217ee
Neffer2 <n3f73r@gmail.com>
2025-01-30 15:47:15 -0500

    1. Correccion codigops

--- database/migrations/2024_12_11_000117_create_codigos_table.php
+++ database/migrations/2024_12_11_000117_create_codigos_table.php
@@ -13,12 +13,13 @@ return new class extends Migration
     {
         Schema::create('codigos', function (Blueprint $table) {
             $table->id();
-            $table->string('codigo')->unique();
+            $table->string('codigo');
             $table->string('serial');
+            $table->integer('conteo');
             $table->foreign('estado_cod')->references('id')->on('estados');
-            $table->foreignId('estado_cod');
+            $table->foreignId('estado_cod')->default(1);
             $table->foreign('estado_serial')->references('id')->on('estados');
-            $table->foreignId('estado_serial');
+            $table->foreignId('estado_serial')->default(1);
             $table->timestamps();
         });
     }
