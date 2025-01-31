76180120dae27f8da6d62e355c9739e9344d2be4
Neffer2 <n3f73r@gmail.com>
2025-01-31 15:54:42 -0500

    1. Avances plataforma Backoffice

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -55,6 +55,7 @@ class Kernel extends HttpKernel
     protected $middlewareAliases = [
         'ruleta' => \App\Http\Middleware\Ruleta::class,
         'shopper' => \App\Http\Middleware\Shopper::class,
+        'backoffice' => \App\Http\Middleware\Backoffice::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
---
+++ app/Livewire/BackOffice/Shopper/Facturas.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\BackOffice\Shopper;
+
+use Livewire\Component;
+
+class Facturas extends Component
+{
+    public function render()
+    {
+        return view('livewire.back-office.shopper.facturas');
+    }
+}
