0420158d2b48c5aaf80cd0ddd57997ba58ff0008
Sebastián Camargo <juansecapri@hotmail.com>
2025-05-06 16:50:45 -0500

    feat: Ajustes de ruleta

--- app/Livewire/RuletaSorteo.php
+++ app/Livewire/RuletaSorteo.php
@@ -15,8 +15,9 @@ class RuletaSorteo extends Component
         $this->participante = DB::table('registro_codigos as rc')
             ->join('codigos as c', 'rc.codigo_id', '=', 'c.id')
             ->join('users as u', 'rc.user_id', '=', 'u.id')
+            ->join('registro_facturas as rf', 'rc.factura_id', '=', 'rf.id')
             ->select('rc.codigo_id', 'c.codigo', 'rc.user_id', 'u.nombre')
-            ->where('rc.estado_id', 1)
+            ->where('rf.estado_id', 1)
             ->where('rc.created_at', '<', '2025-05-08 23:59:59')
             ->inRandomOrder()
             ->first();
