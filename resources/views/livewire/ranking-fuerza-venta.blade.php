db219618c5703c625f9ae4240f6ef2e63d0cda63
Sebastián Camargo <juansecapri@hotmail.com>
2025-05-19 17:53:43 -0500

    feat: ajustes de tabla de fuerza de venta

--- resources/views/livewire/ranking-fuerza-venta.blade.php
+++ resources/views/livewire/ranking-fuerza-venta.blade.php
@@ -16,55 +16,55 @@
         <tr>
             <td>1</td>
             <td>JACKSON GAITÁN</td>
-            <td>485</td>
+            <td>810</td>
         </tr>
         <tr>
             <td>2</td>
-            <td>GERMAN ANDRES ISAZA ARREADONDO</td>
-            <td>460</td>
+            <td>JUAN TUIRAN</td>
+            <td>740</td>
         </tr>
         <tr>
             <td>3</td>
             <td>ANGELO PATRON</td>
-            <td>445</td>
+            <td>665</td>
         </tr>
         <tr>
             <td>4</td>
-            <td>JOSE BOLIVAR</td>
-            <td>395</td>
+            <td>GERMAN ANDRES ISAZA ARREADONDO</td>
+            <td>645</td>
         </tr>
         <tr>
             <td>5</td>
-            <td>DAVID MORAN</td>
-            <td>380</td>
+            <td>JOSE BOLIVAR</td>
+            <td>560</td>
         </tr>
         <tr>
             <td>6</td>
-            <td>MONICA XIMENA LEAL SILVA</td>
-            <td>335</td>
+            <td>DAVID MORAN</td>
+            <td>515</td>
         </tr>
         <tr>
             <td>7</td>
-            <td>DIEGO ARMANDO NAVARRO PABON</td>
-            <td>330</td>
+            <td>MAURICIO SUAREZ</td>
+            <td>490</td>
         </tr>
         <tr>
             <td>8</td>
-            <td>MAURICIO SUAREZ</td>
-            <td>330</td>
+            <td>ANDRES FELIPE LEON BELLO</td>
+            <td>485</td>
         </tr>
         <tr>
             <td>9</td>
-            <td>JUAN TUIRAN</td>
-            <td>320</td>
+            <td>Oscar Andrés Giraldo</td>
+            <td>475</td>
         </tr>
         <tr>
             <td>10</td>
-            <td>Oscar Andrés Giraldo</td>
-            <td>310</td>
+            <td>ARMANDO RUIZ</td>
+            <td>460</td>
         </tr>
         <tr>
-            <td>{{ $user_rank+=1 }}</td>
+            <td>{{ $user_rank += 1 }}</td>
             <td>{{ auth()->user()->nombre }}</td>
             <td>{{ auth()->user()->puntos }}</td>
         </tr>
