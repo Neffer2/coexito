b9f7bd1318813001f72df9f53714a7d41cfdd23e
Sebastián Camargo <juansecapri@hotmail.com>
2025-08-14 11:20:16 -0500

    Ajustes de fechas para sorteo de ruleta

--- app/Livewire/RuletaSorteo.php
+++ app/Livewire/RuletaSorteo.php
@@ -18,7 +18,7 @@ class RuletaSorteo extends Component
             ->join('users as u', 'rc.user_id', '=', 'u.id')
             ->select('rc.codigo_id', 'c.codigo', 'rc.user_id', 'u.nombre')
             ->where('rc.estado_id', 1)
-            ->where('rc.created_at', '<', '2025-05-08 23:59:59')
+            ->where('rc.created_at', '<', '2025-08-15 23:59:59')
             ->inRandomOrder()
             ->first();
 
